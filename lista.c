dfd8ecae0573dcf90d382f62c1eb447f412a1ab0
Isabella Fagioli <isabella.fagioli@gmail.com>
2021-10-27 11:03:22 -0300

    completed task (not tested though)

--- lista.c
+++ lista.c
@@ -26,8 +26,19 @@ pointer put(pointer list, int number){
     return new;
 }
 
+int count_elements(pointer list){
+    pointer aux = list;
+    int counter = 0;
+    while(aux != NULL){
+        counter++;
+        aux = aux->next;
+    }
+    return counter;
+}
+
 int get(pointer list, int pos){
     pointer aux = list;
+    int pos = count_elements(list) - pos; 
     //move in the list until the position asked is found
     for(int i = 0; i < pos; i++){
         aux = aux->next;
@@ -66,25 +77,76 @@ int fisrt(pointer list){
 }
 
 pointer remove(pointer list, int pos){
-    int counter = 1;
+    int element = get(list, pos);
+    printf("%d", element);
     pointer aux_list = list; //just to clear (avoid memory leak)
     ponter new_list = initialize_list();
     while(list != NULL){
-        if(counter != pos){
+        if(list->data != element){
             new_list = put(new_list, list->data);
         }
-        if(counter == pos){
+        if(list->data == element){
             list = list->next;
         }
         list->next;
-        counter++;
     }
     clear(aux_list);
     return new_list;
 }
-
 int last(pointer list){
     return list->data;
 }
 
+void read_entry(char *entry, pointer list) {
+
+    if(entry[0] == 'p'){
+        int number = entry[4] - '0'; //supposing the example that has only numers of one digit
+        put(list, number);
+    }
+    if(entry[0] == 'g'){
+        int pos = entry[4] - '0'; //supposing the example that has only numers of one digit
+        int number = get(list, pos);
+        printf("%d", number);
+    }
+    if(entry[0] == 'l' && entry[1] == 'i'){
+        list(list);
+    }
+    if(entry[0] == 'r'){
+        int pos = entry[7] - '0'; //supposing the example that has only numers of one digit
+        int number = remove(list, pos);
+        list(list);
+    }
+    if(entry[0] == 'c'){
+       clear(list);
+    }
+    if(entry[0] == 'f'){
+        int number = first(list);
+        printf("%d", number);
+    }
+    if(entry[0] == 'l' && entry[1] == 'a'){
+        int number = last(list);
+        printf("%d", number);
+    }	
+}
+
+int main(int argc, char *argv[]) {
+	char input[201];
+    pointer list = initialize_list();
+
+	while(1) {
+		printf("prompt> ");
+		if (fgets(input, 200, stdin) == NULL) {
+			printf("An error ocurred.\n");
+			break;
+		}
+
+		if (strncmp(input, "exit\n", 5) == 0) {
+			printf("Leaving. Good bye.\n");
+			break;
+		}
+
+		read_entry(input, list);
+	}
+
+	return EXIT_SUCCESS;
 }
